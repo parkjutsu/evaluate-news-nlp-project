0e4c87af87b32d3dddc0e8d7c1d2cbe68d89b8e8
Abraham Park <65786838+parkjutsu@users.noreply.github.com>
2021-01-05 23:23:31 -0800

    Add API fetch request from server

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,22 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let formText = document.getElementById('name').value;
+    Client.checkForName(formText);
 
-    // TO-DO: Fetch request that could reference an API for a project
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    // Fetch request to Express server to reference API
+    console.log("::: Form Submitted :::");
+    fetch('/test', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+        body: formText,
+    })
+    .then(res => res.json()) // Parse JSON response into native JavaScript object
+    .then(data => {
+        document.getElementById('results').innerHTML = `MeaningCloud identifies this text as ${data.subjectivity} and ${data.irony} with ${data.confidence}% confidence.`;
     })
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -2,26 +2,56 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
 
-const app = express()
+// node-fetch to use fetch on Node.js runtime
+const fetch = require('node-fetch');
 
-app.use(express.static('dist'))
+const app = express();
 
-console.log(__dirname)
+// Body parser for Express POST requests
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize main project folder
+app.use(express.static('dist'));
+
+console.log(__dirname);
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+    res.sendFile(path.resolve('src/client/views/index.html'));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+    console.log('Example app listening on port 8081!');
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.post('/test', function (req, res) {
+    let data = req.body;
+    console.log(`Server received data:`);
+    console.log(data);
+
+    // Construct the API endpoint
+    const endpoint = `${process.env.API_URL}?key=${process.env.API_KEY}&of=json&txt=${data}&lang=en`;
+    console.log(`Endpoint: ${endpoint}`);
+
+    fetch(endpoint, {
+        method: 'POST',
+        body: data,
+    })
+    .then(response => response.json()) // Parse JSON response into native JavaScript object
+    .then(data => {
+        console.log(data);
+        res.send(data);
+    })
+});
